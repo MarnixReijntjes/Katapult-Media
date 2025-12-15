80ad53a2ccdc616a7881b4808376f66650c5893a
MarnixReijntjes <30289533+MarnixReijntjes@users.noreply.github.com>
2025-12-15 16:01:12 +0100

    Update twilioWebhook.js

--- twilioWebhook.js
+++ twilioWebhook.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const { buildSystemPrompt } = require("./promptBuilder");
+const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Gather input="speech" language="nl-NL" timeout="5" action="/handle-speech" method="POST">
+    <Say language="nl-NL" voice="Polly.Lotte">
+      Goedemiddag, u spreekt met Tessa van Dutch Empire Security. Waar kan ik u mee helpen?
+    </Say>
+  </Gather>
+  <Say language="nl-NL" voice="Polly.Lotte">
+    Ik heb helaas niets gehoord. Kunt u het nog een keer proberen?
+  </Say>
+</Response>`;
 
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-
-app.post("/voice", (req, res) => {
-  // Voor nu hardcoded DES (schaalbaar: later mapping op req.body.To)
-  const systemPrompt = buildSystemPrompt("des");
-
-  // Demo: spreek 1 zin uit + toon dat prompt geladen is (in logs)
-  console.log("Incoming call To:", req.body.To, "From:", req.body.From);
-  console.log(systemPrompt);
+app.post("/handle-speech", (req, res) => {
+  const speech = req.body.SpeechResult;
+  console.log("User said:", speech);
 
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Say language="nl-NL" voice="Polly.Lotte">
-    Goedemiddag, u spreekt met Tessa, de digitale receptionist van Dutch Empire Security. Waar kan ik u mee helpen?
+    Dank u. Ik ga dit doorgeven aan een medewerker.
   </Say>
 </Response>`;
 
   res.type("text/xml").send(twiml);
 });
-
-module.exports = { app };
